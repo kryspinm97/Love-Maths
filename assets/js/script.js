bf4babe6a41c5c28c32f00ae3cad9666a6257164
kryspinm97 <maneskryspin69@gmail.com>
2022-10-02 07:46:57 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,12 +47,14 @@ function checkAnswer() {
 
   if(isCorrect) {
     alert("Hey! You got it right! :)");
+    incrementScore();
   } else {
     alert(`Awww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    incrementWrongAnswer();
   }
 
   runGame(calculatedAnswer[1]);
-  
+
 }
 
 /**
@@ -72,12 +74,20 @@ function calculateCorrectAnswer() {
   }
 }
 
+/**
+ * Gets the correct score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+ let oldScore = parseInt(document.getElementById("score").innerText);
+ document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the incorrect score from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+  let oldScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
